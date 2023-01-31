b0b675de6c292f3b8e5bfc03bccbd6a66dac9143
Eldar Galimshin <63157399+n1ghtowl88@users.noreply.github.com>
2023-01-31 14:10:34 +0400

    Update settings.py

--- notificatonService/settings.py
+++ notificatonService/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ar-xj%hh)#-b!vtk920n7t^kw-!bli=y2i@b3w(8q8v+mdofs@'
+SECRET_KEY = '12345'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
